50e6f574783ab296a5cc9ad18b6b294d74a368b1
LuObreg <LuObreg@users.noreply.github.com>
2021-07-15 23:01:35 -0300

    Ejercicio 1 - comentarios

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Ejercicio 1
+//Crear una loop for que muestre en consola la tabla del 5
 const multiplicando = 5;
 for(let i = 1; i < 11; i++){
     console.log (`${multiplicando} x ${i} = ${i * multiplicando}`);
