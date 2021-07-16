299a87164c7cbf9f1c4300ad47cdd5d108231a9d
LuObreg <LuObreg@users.noreply.github.com>
2021-07-16 19:26:24 -0300

    Ejercicio 3

--- act2.js
+++ act2.js
@@ -1,15 +1,2 @@
-//Ejercicio 2
-//Modificar el ejercicio anterior usando File System para grabar la tabla del 5 en un archivo .txt
-
-const multiplicando = 5;
-let result = "";
-for(let i = 1; i < 11; i++){
-    result += `${multiplicando} x ${i} = ${i * multiplicando} \n`;
-}
-console.log(result);
-
-const fs = require('fs');
-
-fs.writeFile('tabla5.txt', result, function (err) {
-    if (err) return console.log(err);
-  });
+const tabla = require('./act3');
+tabla.printTabla();
---
+++ act3.js
@@ -0,0 +1,25 @@
+//Ejercicio 2
+//Modificar el ejercicio anterior usando File System para grabar la tabla del 5 en un archivo .txt
+const fs = require('fs');
+
+var result = "";
+function getTabla(){
+    const multiplicando = 5;
+
+    for(let i = 1; i < 11; i++){
+        result += `${multiplicando} x ${i} = ${i * multiplicando} \n`;
+    }
+    return Promise.resolve(result);
+}
+
+function printTabla(){
+    getTabla()
+        .then(
+            fs.writeFile('tabla5.txt', result, function (err) {
+                if (err) return console.log(err);
+            })
+        )
+        .catch(console.log("No pudo realizarse la operación"));  
+}
+
+module.exports = { printTabla };
