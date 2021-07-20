faa2838d4f4a11741af3f2a95d88b3dc691d8dd4
LuObreg <LuObreg@users.noreply.github.com>
2021-07-20 19:24:07 -0300

    Ejercicio 7

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,8 +1,24 @@
 //Ejercicio 3, se importa el ejercicio anterior luego de haber convertido la función a una promesa
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
-
 const tabla = require('./ejercicio4');
-var multiplicando = argv.base;
+
+const argv = require('yargs')
+                .option('b',{
+                alias: 'base',
+                type: 'number',
+                demandOption: true
+            })
+                .check((argv) => {
+                    if (argv.b < 1 || argv.b > 20){
+                        throw new Error("Debes colocar un valor de base entre 1 y 20");
+                    }
+                    else{
+                        return true
+                    }
+                })
+            .argv;
+console.clear();
+console.log(argv);
+console.log('base: yargs', argv.b)
+
+var multiplicando = argv.b;
 tabla.printTabla(multiplicando);
