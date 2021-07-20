82d8e136e8369b21c5b7e07a2d3087cdd349fb5e
LuObreg <LuObreg@users.noreply.github.com>
2021-07-20 14:23:00 -0300

    Ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,10 @@
+//Ejercicio 5
+//Crear una loop for que muestre en consola la tabla del 5
+
+var colors = require('colors');
+
+const multiplicando = 5;
+console.log('La tabla del 5'.rainbow); // rainbow
+for(let i = 1; i < 11; i++){
+    console.log (`${multiplicando} x ${i} = ${i * multiplicando}`);
+}
