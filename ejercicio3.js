44f739ae4a11afa4a38e12799d67f9a3c83d508f
LuObreg <LuObreg@users.noreply.github.com>
2021-07-20 14:32:13 -0300

    Ejercicio 6

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,4 +1,8 @@
 //Ejercicio 3, se importa el ejercicio anterior luego de haber convertido la función a una promesa
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
 const tabla = require('./ejercicio4');
-var multiplicando = process.argv[2].slice(-1);
+var multiplicando = argv.base;
 tabla.printTabla(multiplicando);
